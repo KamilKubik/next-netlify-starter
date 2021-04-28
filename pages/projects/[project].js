3cf0530ddf50d33f8ac8ccbc11c038d3f132c6e8
KamilKubik <robekub15@wp.pl>
2021-04-28 21:08:48 +0200

    Fix mistype

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -3,7 +3,7 @@ import React from 'react';
 import { db } from '../../lib/firebase-admin';
 
 const RandomProject = () => {
-  console.log(projects);
+  // console.log(projects);
   return <div>Random Project</div>;
 };
 
