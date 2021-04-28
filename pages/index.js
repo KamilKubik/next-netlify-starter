608b2664252d6deab7ab32bb52d3f13a1fb6bd69
KamilKubik <robekub15@wp.pl>
2021-04-28 21:02:00 +0200

    Add getStaticProps & getStaticPaths functionality

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
+import Link from 'next/link';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-import Link from 'next/link';
+import { db } from '../lib/firebase';
 
 export default function Home() {
+  const router = useRouter();
+
+  const [inputValue, setInputValue] = useState('');
+  useEffect(async () => {
+    const snapshot = await db.collection('projects').get();
+
+    const projects = [];
+    snapshot.forEach((doc) => {
+      projects.push({ id: doc.id, ...doc.data() });
+    });
+    console.log(projects);
+  });
+
+  const onProjectCreate = async () => {
+    const projectId = uuidv4();
+    const newProject = {
+      projectId,
+      projectName: inputValue,
+    };
+    await db.collection('projects').doc(projectId).set(newProject);
+    router.push('/projects/[project]', `/projects/random${inputValue}`);
+  };
+
   return (
     <div className="container">
       <Head>
@@ -14,6 +41,8 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">Hello from new deploy</p>
+        <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <button onClick={onProjectCreate}>Go to your project!</button>
         <Link href="/projects/[project]" as="/projects/random">
           <a>Go to random project</a>
         </Link>
--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -1,9 +1,62 @@
 import React from 'react';
+// import { getAllProjects } from '../../lib/db-admin';
+import { db } from '../../lib/firebase-admin';
 
-const RandomProject = ({ params }) => {
+const RandomProject = ({ projects }) => {
+  console.log(projects);
   return <div>Random Project</div>;
 };
 
-const getServerSideProps = async () => {};
+export const getStaticPaths = async () => {
+  // const { projects } = await getAllProjects();
+  const snapshot = await db.collection('projects').get();
+
+  const projects = [];
+  snapshot.forEach((doc) => {
+    projects.push(doc.data());
+  });
+
+  const paths = projects.map((project) => ({
+    params: {
+      project: project.projectName.toString(),
+    },
+  }));
+
+  return {
+    paths: paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  // const { projects } = await getParticularProject(params.work);
+  let ref = db.collection('projects').where('projectName', '==', params.project);
+
+  const snapshot = await ref.get();
+  const projects = [];
+
+  snapshot.forEach((doc) => {
+    projects.push({ id: doc.id, ...doc.data() });
+  });
+
+  return {
+    props: { projects },
+    revalidate: 1,
+  };
+};
+
+// export const getServerSideProps = async () => {
+//   // const { projects } = getAllProjects();
+//   const snapshot = await db.collection('projects').get();
+
+//   const projects = [];
+//   snapshot.forEach((doc) => {
+//     projects.push({ id: doc.id, ...doc.data() });
+//   });
+
+//   return {
+//     props: { projects },
+//   };
+// };
 
 export default RandomProject;
