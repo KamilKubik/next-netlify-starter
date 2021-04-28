f0e71ec1cc93b9e36dab610af5d42c2db82d595e
KamilKubik <robekub15@wp.pl>
2021-04-28 21:08:15 +0200

    Try to fix production problem

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -2,48 +2,48 @@ import React from 'react';
 // import { getAllProjects } from '../../lib/db-admin';
 import { db } from '../../lib/firebase-admin';
 
-const RandomProject = ({ projects }) => {
+const RandomProject = () => {
   console.log(projects);
   return <div>Random Project</div>;
 };
 
-export const getStaticPaths = async () => {
-  // const { projects } = await getAllProjects();
-  const snapshot = await db.collection('projects').get();
+// export const getStaticPaths = async () => {
+//   // const { projects } = await getAllProjects();
+//   const snapshot = await db.collection('projects').get();
 
-  const projects = [];
-  snapshot.forEach((doc) => {
-    projects.push(doc.data());
-  });
+//   const projects = [];
+//   snapshot.forEach((doc) => {
+//     projects.push(doc.data());
+//   });
 
-  const paths = projects.map((project) => ({
-    params: {
-      project: project.projectName.toString(),
-    },
-  }));
+//   const paths = projects.map((project) => ({
+//     params: {
+//       project: project.projectName.toString(),
+//     },
+//   }));
 
-  return {
-    paths: paths,
-    fallback: true,
-  };
-};
+//   return {
+//     paths: paths,
+//     fallback: true,
+//   };
+// };
 
-export const getStaticProps = async ({ params }) => {
-  // const { projects } = await getParticularProject(params.work);
-  let ref = db.collection('projects').where('projectName', '==', params.project);
+// export const getStaticProps = async ({ params }) => {
+//   // const { projects } = await getParticularProject(params.work);
+//   let ref = db.collection('projects').where('projectName', '==', params.project);
 
-  const snapshot = await ref.get();
-  const projects = [];
+//   const snapshot = await ref.get();
+//   const projects = [];
 
-  snapshot.forEach((doc) => {
-    projects.push({ id: doc.id, ...doc.data() });
-  });
+//   snapshot.forEach((doc) => {
+//     projects.push({ id: doc.id, ...doc.data() });
+//   });
 
-  return {
-    props: { projects },
-    revalidate: 1,
-  };
-};
+//   return {
+//     props: { projects },
+//     revalidate: 1,
+//   };
+// };
 
 // export const getServerSideProps = async () => {
 //   // const { projects } = getAllProjects();
